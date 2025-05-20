1bb8501fa85eb4286d1effbe002113e6a28d199c
kurunin52 <33592045+kurunin52@users.noreply.github.com>
2025-05-20 11:20:34 +0000

    feat: create repository to order

--- model/src/order.rs
+++ model/src/order.rs
@@ -1,23 +1,30 @@
 use serde::{Deserialize, Serialize};
-use sqlx::{prelude::FromRow, PgPool};
+use sqlx::prelude::FromRow;
 
-#[derive(Serialize, Deserialize, FromRow)]
+#[derive(Serialize, Deserialize, FromRow, Clone)]
 pub struct OrderBalance {
-    order_id: i32,
-    order_status: String,
+    pub order_id: i32,
+    pub order_status: String,
 }
 
-pub async fn search_order_balance(pool: &PgPool, status: &String) -> Vec<OrderBalance> {
-    sqlx::query_as("SELECT order_id, order_status FROM orders WHERE order_status = $1")
-        .bind(status)
-        .fetch_all(pool)
-        .await
-        .unwrap()
+pub type OrderError = String;
+pub type OrderResult<T> = Result<T, OrderError>;
+
+#[async_trait::async_trait]
+pub trait OrderRepository: Send + Sync + 'static {
+    async fn get_order_balance_list(&self) -> OrderResult<Vec<OrderBalance>>;
+    async fn search_order_balance(&self, status: &str) -> OrderResult<Vec<OrderBalance>>;
 }
 
-pub async fn get_order_balance_list(pool: &PgPool) -> Vec<OrderBalance> {
-    sqlx::query_as("SELECT order_id, order_status FROM orders")
-        .fetch_all(pool)
-        .await
-        .unwrap()
+pub async fn search_order_balance(
+    order_repository: &impl OrderRepository,
+    status: &str,
+) -> OrderResult<Vec<OrderBalance>> {
+    order_repository.search_order_balance(status).await
+}
+
+pub async fn get_order_balance_list(
+    order_repository: &impl OrderRepository,
+) -> OrderResult<Vec<OrderBalance>> {
+    order_repository.get_order_balance_list().await
 }
